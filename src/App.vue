ae6354a3ec1c066472b0151093dfe616936730bc
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-16 20:54:43 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import {ref, onMounted} from "vue";
+import {Phormal, useLength, useEmail, useRequired} from "@phormal/core";
+
+const formFields = {
+  paymentMethod: {
+    type: 'radiogroup',
+    label: 'Payment Method',
+    value: 'paypal',
+    options: [
+      {label: 'PayPal', value: 'paypal'},
+      {label: 'Credit card', value: 'creditcard'},
+      {label: 'Klarna', value: 'klarna'},
+    ]
+  },
+  name: {
+    label: 'Name',
+    hooks: [useRequired(), useLength(3)]
+  },
+  email: {
+    label: 'Email',
+    hooks: [useRequired(), useEmail()]
+  },
+  newsletter: {
+    type: 'checkbox',
+    label: 'Newsletter',
+    value: true
+  },
+}
+
+const formConfig = {
+  theme: 'basic',
+  el: '#phormal',
+  language: 'en'
+}
+
+const phormal = ref(null);
+
+onMounted(() => {
+  phormal.value = new Phormal(formFields, formConfig);
+});
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Phormal</h1>
+
+    <div id="phormal"></div>
+  </div>
+</template>
+
+<style>
+@import '@phormal/theme-basic/dist/index.css';
+</style>
